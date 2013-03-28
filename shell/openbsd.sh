87789a08434dcc59287e0c99146b7f8b092a29c9
Mark Wong <markwkm@gmail.com>
2013-03-27 17:14:09 -0700

    Update openbsd provisioning script

--- shell/openbsd.sh
+++ shell/openbsd.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# Packages required to build postgres
+
 (cd /usr/ports/devel/git && make install && make clean && make clean=dist && \
 		make clean=depends) || exit 1
 (cd /usr/ports/misc/screen && make install && make clean && make clean=dist && \
@@ -15,4 +17,15 @@
 (cd /usr/ports/textproc/docbook && make install && make clean && \
 		make clean=dist && make clean=depends) || exit 1
 (cd /usr/ports/textproc/libxslt && make install && make clean && \
+		make clean=dist && make clean=depends) || exit 1
+
+# General development tools
+
+(cd /usr/ports/editors/nano && make install && make clean && \
+		make clean=dist && make clean=depends) || exit 1
+(cd /usr/ports/editors/vim && make install && make clean && \
+		make clean=dist && make clean=depends) || exit 1
+(cd /usr/ports/editors/emacs23 && make install && make clean && \
+		make clean=dist && make clean=depends) || exit 1
+(cd /usr/ports/devel/ectags && make install && make clean && \
 		make clean=dist && make clean=depends) || exit 1
